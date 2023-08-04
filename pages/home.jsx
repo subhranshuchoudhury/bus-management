c4d23007eb5277806ee06ea3de5df22ed3bdf039
Subhranshu Choudhury <subhransuchoudhury00@gmail.com>
2023-08-04 14:09:20 +0530

    ui upgrade stage 2

--- pages/home.jsx
+++ pages/home.jsx
@@ -1,22 +1,14 @@
 import React, { useState, useEffect } from "react";
-import {
-  View,
-  Text,
-  ScrollView,
-  FlatList,
-  TouchableOpacity,
-} from "react-native";
+import { View, Text, ScrollView, FlatList } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
-import { Skeleton, SearchBar } from "@rneui/themed";
-import { searchBarPlaceholderText, beginsEnds } from "../utils/texts/texts";
 import { getAllBus } from "../utils/hygraph/connect";
+import BusRowCard from "../components/BusRowCard";
+import SkeletonBox from "../components/SkeletonBox";
+import SearchBarComp from "../components/SearchBar";
 import socketServices from "../utils/socket/socketService";
-import { Image } from "react-native";
-// import { SearchBar } from "react-native-elements";
-// import { LinearGradient } from "expo-linear-gradient";
+import { T3 } from "../utils/texts/texts";
 
 const Home = () => {
-  const [search, setSearch] = useState("");
   const [Buses, setBuses] = useState([]);
   const [socketData, setSocketData] = useState("");
 
@@ -39,41 +31,20 @@ const Home = () => {
       });
   };
 
+  const searchInput = (val) => {
+    alert(val);
+  };
+
   // end defined functions
   return (
     <SafeAreaView>
       <View className="flex bg-white">
         <View>
-          <SearchBar
-            lightTheme={true}
-            showLoading={false}
-            className="rounded-lg"
-            round
-            searchIcon={() => (
-              <Image
-                className="w-8 h-8"
-                source={require("../assets/icons/marker.png")}
-              />
-            )}
-            // searchIcon={{ size: 30, color: "black" }}
-            onChangeText={(text) => {
-              setSearch(text);
-            }}
-            onClear={(text) => {
-              setSearch("");
-            }}
-            placeholder={searchBarPlaceholderText}
-            value={search}
-            containerStyle={{
-              backgroundColor: "white",
-              padding: 20,
-              borderColor: "white",
-            }}
-            inputContainerStyle={{ backgroundColor: "#eee", height: 60 }}
-            inputStyle={{ color: "black" }}
-          />
+          <SearchBarComp searchInput={searchInput} />
         </View>
-
+        <View className="ml-5 mt-5">
+          <Text className="text-xl font-bold">{T3}</Text>
+        </View>
         <View className="justify-center items-center space-y-6 mt-6 mb-48">
           <FlatList
             className="w-[90%]"
@@ -85,84 +56,6 @@ const Home = () => {
         </View>
       </View>
     </SafeAreaView>
-  );
-};
-
-const BusRowCard = ({ props }) => {
-  const { active, startTime, endTime, vehicleNo, vendor, id, routes } = props;
-
-  const convertToTime = (date) => {
-    const newDate = new Date(date);
-    const formattedDate = newDate.toLocaleString("en-US", {
-      hour: "numeric",
-      minute: "numeric",
-      hour12: true,
-    });
-    return formattedDate;
-  };
-
-  return (
-    <TouchableOpacity>
-      <View className="flex bg-slate-100 m-1 mb-4 rounded-md shadow-sm shadow-black">
-        <View className="flex-row justify-between items-center p-3">
-          <Image
-            className="w-16 h-16"
-            source={require("../assets/icons/school-bus.png")}
-          />
-          <View className="bg-yellow-300 h-9 p-2 rounded-lg min-w-[30%] shadow-sm shadow-black">
-            <Text className="text-md font-bold text-center">{vehicleNo}</Text>
-          </View>
-        </View>
-        <View className="flex-col p-2 pl-4 pr-4">
-          <View className="flex-row justify-between items-center mb-4">
-            <Text className="font-bold">{beginsEnds[0]}</Text>
-            <Skeleton
-              width={100}
-              height={5}
-              style={{ borderRadius: 40 }}
-            ></Skeleton>
-            <Text className="font-bold">{beginsEnds[1]}</Text>
-          </View>
-          {routes.flatMap((d) => {
-            return (
-              <View className="flex-row justify-between" key={d?.id}>
-                <Text>{d?.route?.split("-")[0]}</Text>
-                <Text>{d?.route?.split("-")[1]}</Text>
-              </View>
-            );
-          })}
-        </View>
-        <View>
-          <View className="justify-between items-center flex-row p-2 pl-4 pr-4">
-            <Text>{convertToTime(startTime)}</Text>
-            <Text>{convertToTime(endTime)}</Text>
-          </View>
-        </View>
-      </View>
-    </TouchableOpacity>
-  );
-};
-
-const SkeletonBox = () => {
-  const demoArray = [...Array(7).keys()].map((x) => x * 2);
-  return (
-    <View className="justify-center items-center space-y-5">
-      {demoArray.flatMap((index) => {
-        return (
-          <Skeleton
-            key={index}
-            LinearGradientComponent={() => (
-              <View className="rotate-12">
-                <View className="w-16 h-52 bg-slate-200"></View>
-              </View>
-            )}
-            animation="wave"
-            width={"95%"}
-            height={80}
-          />
-        );
-      })}
-    </View>
   );
 };
 
--- utils/texts/texts.js
+++ utils/texts/texts.js
@@ -1,8 +1,9 @@
 // home.jsx
 
-const searchBarPlaceholderText = "Where to?";
-const beginsEnds = ["Begins", "Ends"];
+const T1 = "Where to?"; // Search Bar
+const T2 = ["Begins", "Ends"]; // Bus Card
+const T3 = "All Buses"; // Home Screen
 
 // export
 
-module.exports = { searchBarPlaceholderText, beginsEnds };
+module.exports = { T1, T2, T3 };
